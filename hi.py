1e9908556cc6809861fae76a6e87e14f6aac0f96
Dinira Pathirana <128203626+dexa20@users.noreply.github.com>
2023-10-28 18:36:13 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,2 +1,21 @@
-print('hi')
+import random
+import time
 
+def generate_colorful_pattern():
+    colors = ['\033[91m', '\033[92m', '\033[93m', '\033[94m', '\033[95m', '\033[96m']
+    reset = '\033[0m'
+    
+    while True:
+        for _ in range(10):
+            row = ''
+            for _ in range(20):
+                color = random.choice(colors)
+                character = chr(random.randint(33, 126))
+                row += f'{color}{character}{reset} '
+            print(row)
+            time.sleep(0.1)
+        
+        print('\n' * 10)  # Clear the screen
+
+if __name__ == '__main__':
+    generate_colorful_pattern()
